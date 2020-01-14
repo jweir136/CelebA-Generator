ce0e2fd77b5d82b31085e342015f7f443e5bab22
jweir136 <jweir136@yahoo.com>
2020-01-14 10:03:54 -0500

    update

--- CelebAVAE.py
+++ CelebAVAE.py
@@ -6,8 +6,20 @@ import numpy as np
 import pandas as pd
 import os
 
+class Flatten(nn.Module):
+  def forward(self, x):
+    batch_size = x.size()[0]
+    return x.view(batch_size, 128*84*84)
+
+class UnFlatten(nn.Module):
+  def forward(self, x):
+    batch_size = x.size()[0]
+    return x.view(batch_size, 128, 84, 84)
+
 class CelebAVAE(nn.Module):
   def __init__(self):
+    super().__init__()
+
     self.encoder = nn.Sequential(
       nn.Conv2d(3, 16, 5),
       nn.ReLU(True),
@@ -17,11 +29,16 @@ class CelebAVAE(nn.Module):
       nn.ReLU(True),
       nn.Conv2d(64, 128, 5),
       nn.ReLU(True),
-      nn.Flatten()
+      Flatten(),
+      nn.Linear(128*84*84, 120),
+      nn.ReLU(True)
     )
-    
+    self.mu_layer = nn.Linear(120, 2)
+    self.logvar_layer = nn.Linear(120, 2)
+
   def encode(self, x):
-    return self.encoder(x)
+    x = self.encoder(x)
+    return x
 
   def forward(self, x):
     return x
--- main.py
+++ main.py
@@ -11,34 +11,31 @@ from tqdm import tqdm
 from CelebADataset import *
 from CelebAVAE import *
 
+###################### CREATE THE TRANSFORMATIONS, DATASETS, AND DATA LOADERS #####################
+
 df = pd.read_csv("../../datasets/celebA/list_attr_celeba.csv")
 img_dir = "../../datasets/celebA/img_align_celeba/img_align_celeba"
 
+mean = np.load("mean.npy")
+std = np.load("std.npy")
+
 trans = transforms.Compose([
-  transforms.Resize(100),
-  transforms.ToTensor()
+  transforms.Resize((100, 100)),
+  transforms.ToTensor(),
+  transforms.Normalize(mean=mean, std=std)
 ])
 
-trainset = CelebADataset(img_dir, df, trans, training=True)
-testset = CelebADataset(img_dir, df, trans, training=False)
-trainloader = data.DataLoader(trainset, batch_size=128, num_workers=12)
+trainset = CelebADataset(img_dir, df, trans, training=True, shuffle=True)
+testset = CelebADataset(img_dir, df, trans, training=False, shuffle=True)
+trainloader = data.DataLoader(trainset, batch_size=1, num_workers=12)
 testloader = data.DataLoader(testset, batch_size=128, num_workers=12)
 
-mean = 0.0
-for images, _ in tqdm(trainloader):
-    batch_samples = images.size(0) 
-    images = images.view(batch_samples, images.size(1), -1)
-    mean += images.mean(2).sum(0)
+###################### LOAD THE MODEL ##############################################################
 
-mean = mean / len(trainloader.dataset)
+vae = CelebAVAE()
+vae = vae.cuda()
 
-var = 0.0
-for images, _ in tqdm(trainloader):
-    batch_samples = images.size(0)
-    images = images.view(batch_samples, images.size(1), -1)
-    var += ((images - mean.unsqueeze(1))**2).sum([0,2])
-
-std = torch.sqrt(var / (len(trainloader.dataset)*100*100))
-
-np.save("mean.npy", mean)
-np.save("std.npy", std)
+for x, _ in trainloader:
+  x = x.cuda().float()
+  print(vae.encode(x).size())
+  break
