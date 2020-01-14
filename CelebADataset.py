1dae52a39e0f090b6518a53bd1605ff3b370240e
jweir136 <jweir136@yahoo.com>
2020-01-14 09:44:06 -0500

    fix syntax error with shuffle() method

--- CelebADataset.py
+++ CelebADataset.py
@@ -2,7 +2,7 @@ import torch
 import torch.utils.data as data
 from PIL import Image
 import os
-from sklearn.utils import shuffle
+import sklearn.utils as utils
 
 class CelebADataset(data.Dataset):
   def __init__(self, img_dir, df, transforms, training=True, shuffle=False):
@@ -16,7 +16,7 @@ class CelebADataset(data.Dataset):
       self.filenames = os.listdir(self.img_dir)[150000:]
 
     if shuffle:
-      self.filenames = shuffle(self.filenames)
+      self.filenames = utils.shuffle(self.filenames)
 
   def __len__(self):
     return len(self.filenames)
@@ -24,5 +24,5 @@ class CelebADataset(data.Dataset):
   def __getitem__(self, idx):
     img = Image.open(os.path.join(self.img_dir, self.filenames[idx]))
     X = self.transforms(img)
-    Y = self.df.loc[self.df['image_id'] == self.filenames[idx]].values[:, 1:]
+    Y = torch.tensor(self.df.loc[self.df['image_id'] == self.filenames[idx]].drop(['image_id'], axis=1).values)
     return X, Y
